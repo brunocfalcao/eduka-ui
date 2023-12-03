0b3862b89f00daaf30cd08bdff9a7ccef1a3e704
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-12-03 14:42:58 +0100

    Initial commit

---
+++ src/EdukaUIServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Brunocfalcao\ZeptoMailApiDriver;
+
+use Illuminate\Support\ServiceProvider;
+
+class EdukaUIServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        //
+    }
+
+    public function register()
+    {
+        //
+    }
+}
