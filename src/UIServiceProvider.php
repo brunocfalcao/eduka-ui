940bde95bf08b2a7430c1e2ce6ddfe4c6acd47be
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2024-03-08 23:41:25 +0100

    Update UIServiceProvider.php

--- src/UIServiceProvider.php
+++ src/UIServiceProvider.php
@@ -2,13 +2,22 @@
 
 namespace Eduka\UI;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class UIServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        //
+        /**
+         * Adds all the <meta name="xxx" value="yyy" />
+         */
+        Blade::directive('ui_meta_names', function () {
+            return '<?php foreach(Nereus::course()->meta_names as $tag => $value): ?>
+<meta name="<?= e($tag) ?>" content="<?= e($value) ?>" />
+<?php endforeach; ?>
+<link rel="canonical" href="https://silver-surfer.masteringnova.com/" />';
+        });
     }
 
     public function register()
