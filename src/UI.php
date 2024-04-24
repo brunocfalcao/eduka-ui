90998fc754b1d73437cbcd795a882b6d13160e7a
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2024-04-24 04:42:05 +0200

    wip

--- src/UI.php
+++ src/UI.php
@@ -2,10 +2,71 @@
 
 namespace Eduka\UI;
 
+use Eduka\Nereus\Facades\Nereus;
+
 class UI
 {
+    private $model;
+
     public function __construct()
     {
         //
+    }
+
+    public function renderHeaders()
+    {
+        $context = Nereus::context();
+
+        $this->renderTitle();
+
+        if ($context['type'] == 'course') {
+            $this->model = Nereus::course();
+
+            $this->renderMetas();
+        }
+
+        $this->renderClarityJS();
+    }
+
+    private function renderClarityJS()
+    {
+        $code = Nereus::course()->clarity_code;
+        echo <<<HTML
+<!-- clarity -->
+<script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "$code");
+</script>
+<!-- /clarity -->
+HTML;
+    }
+
+    private function renderTitle()
+    {
+        $course = Nereus::course()->name;
+        echo <<<HTML
+        <title>$course</title>\n
+HTML;
+    }
+
+    private function renderMetas()
+    {
+        $metaTags = $this->model->metas;
+
+        $html = '';
+
+        foreach ((array) $metaTags as $key => $value) {
+            // 'name|twitter:description' => $this->description,
+            $parts = explode('|', $key);
+            $tagName = $parts[0];
+            $tagValue = $parts[1];
+            $content = $value;
+            echo <<<HTML
+            <meta $tagName="$tagValue" content="$content" />\n
+HTML;
+        }
     }
 }
--- src/UIServiceProvider.php
+++ src/UIServiceProvider.php
@@ -3,20 +3,12 @@
 namespace Eduka\UI;
 
 use Illuminate\Support\ServiceProvider;
-use ImLiam\BladeHelper\Facades\BladeHelper;
 
 class UIServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        /**
-         * Adds all the <meta name="xxx" value="yyy" />
-         */
-        /*
-        BladeHelper::directive('ui_metas', function ($model) {
-            return BladeDirectives::metas($model);
-        });
-        */
+        //
     }
 
     public function register()
